a8fde6ee859ed375873ad620f1bc7b6407ad5b8d
valeriacadena <115510653+valeriacadena@users.noreply.github.com>
2025-08-30 05:10:26 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: '//https://artemisa-one.vercel.app', // solo permite tu frontend
+  origin: 'https://artemisa-one.vercel.app', // solo permite tu frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
